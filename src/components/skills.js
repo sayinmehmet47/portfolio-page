89f73f387c1a9bef9e37cd0cf1ee5222fa3ea152
sayin47 <sayinmehmet47@gmail.com>
2021-10-07 22:36:23 +0300

    portfolio page created

---
+++ src/components/skills.js
@@ -0,0 +1,32 @@
+import React from "react"
+import data from "../yourdata"
+
+const Skills = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="skills-container">
+          <h1>Skills&Certificates</h1>
+          <div className="skills-grid">
+            {data.skills.map((skill, index) => (
+              <div className="skill" key={index}>
+                <img src={skill.img} alt="css"></img>
+                <div style={{ display: "flex" }}>
+                  {skill.icon ? (
+                    <a href={skill.link} style={{ display: "flex" }}>
+                      <img width="25px" src={skill.icon} alt="" />
+                      {""}
+                      <p style={{ marginLeft: "3px" }}>{skill.description}</p>
+                    </a>
+                  ) : null}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
---
+++ src/yourdata.js
@@ -0,0 +1,214 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+import carApp from "./images/car-app.png"
+import pomodoro from "./images/pomodoro-clock.png"
+import quiz from "./images/quiz1.png"
+import todo from "./images/todo.png"
+import intuit from "./images/intuit-clone.png"
+import product from "./images/product-page.png"
+import newyorktimes from "./images/newyork-times.png"
+import drumMachine from "./images/drum-machine.png"
+import stackoverflow from "./images/stackoverflow.svg"
+import linkedin from "./images/linkedin.svg"
+import freecodecamp from "./images/freecodecamp.svg"
+import mongodb from "./images/mongodb.svg"
+import nodejs from "./images/nodejs.svg"
+import mehmetfoto from "./images/mehmetfoto.png"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "mehmet",
+  headerTagline: ["ReactJs Developer", "MERN Stack Developer,"],
+  //   Header Paragraph
+  headerParagraph:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+
+  //Contact Email
+  contactEmail: "sayinmehmet47@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Car Application", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: carApp,
+
+      //Project URL - Add Your Project Url Here
+      url: "https://car-app1.herokuapp.com/",
+    },
+    {
+      title: "Product Landing Page", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: product,
+      //Project URL - Add Your Project Url Here
+      url: "https://productlist-sayin.netlify.app/",
+    },
+    {
+      title: "Pomodoro Clock", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: pomodoro,
+      //Project URL - Add Your Project Url Here
+      url: "https://hackers-timer.netlify.app/",
+    },
+    {
+      title: "General Culture Quiz", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: quiz,
+      //Project URL - Add Your Project Url Here
+      url: "https://tender-raman-f983f3.netlify.app/",
+    },
+    {
+      title: "Intuit Clone", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: intuit,
+      //Project URL - Add Your Project Url Here
+      url: "https://thirsty-pike-7951b9.netlify.app/",
+    },
+    {
+      title: "Todo Application", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: todo,
+      //Project URL - Add Your Project Url Here
+      url: "https://admiring-jang-822de5.netlify.app/",
+    },
+    {
+      title: "NewyorkTimes cloned", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: newyorktimes,
+      //Project URL - Add Your Project Url Here
+      url: "https://priceless-cori-a4ff2a.netlify.app/",
+    },
+    {
+      title: "Drum Machine", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc: drumMachine,
+      //Project URL - Add Your Project Url Here
+      url: "https://adoring-feynman-c1b41e.netlify.app/",
+    },
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "I'm Mehmet. As a Frontend Developer, I`m very passionate and talented about ReactJS, NodeJS, and MongoDB. I devoted myself to learn, implement, adapt and create through my life to build smart web apps to help people and improve life quality around the world. ",
+  aboutParaTwo:
+    "As a Junior Web Developer with 9 months of experience able to build web applications from the ground up - information gathering, planning, design, development, testing and delivery, and maintenance.",
+  aboutImage: mehmetfoto,
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      icon: freecodecamp,
+      description: "Responsive Web Design",
+      link:
+        "https://www.freecodecamp.org/certification/fcc5a51f0ea-4f77-4951-b274-a104a1479f22/responsive-web-design",
+    },
+    {
+      img: cssIcon,
+      icon: freecodecamp,
+      description: "Responsive Web Design",
+
+      link:
+        "https://www.freecodecamp.org/certification/fcc5a51f0ea-4f77-4951-b274-a104a1479f22/responsive-web-design",
+      description: "Responsive Web Design",
+    },
+    {
+      img: jsIcon,
+      icon: freecodecamp,
+      description: "JavaScript Algorithms and Data Structures",
+
+      link:
+        "https://www.freecodecamp.org/certification/fcc5a51f0ea-4f77-4951-b274-a104a1479f22/javascript-algorithms-and-data-structures",
+    },
+    {
+      img: reactIcon,
+      icon: freecodecamp,
+      description: "Front End Libraries",
+      link:
+        "https://www.freecodecamp.org/certification/fcc5a51f0ea-4f77-4951-b274-a104a1479f22/front-end-development-libraries",
+    },
+    {
+      img: mongodb,
+    },
+    {
+      img: nodejs,
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/sayinmehmet47" },
+    {
+      img: linkedin,
+      url: "https://www.linkedin.com/in/sayinmehmet47/",
+    },
+    {
+      img: stackoverflow,
+      url: "https://stackoverflow.com/users/15106423/sayinmehmet47",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
